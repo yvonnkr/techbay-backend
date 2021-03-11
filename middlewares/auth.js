475dd7c47f916062bdd78daaa182ff30552e278d
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2021-03-11 13:56:00 +0000

    Admin check middleware & current-admin route

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const admin = require("../firebase/index");
+const User = require("../models/user");
 
 exports.authCheck = async (req, res, next) => {
   //   console.log(req.headers); next()
@@ -14,7 +15,27 @@ exports.authCheck = async (req, res, next) => {
     next();
   } catch (error) {
     res.status(401).json({
-      error: "Invalid or expired token",
+      error: "Access denied. Invalid or expired token",
+    });
+  }
+};
+
+exports.adminCheck = async (req, res, next) => {
+  const { email } = req.user;
+
+  try {
+    const adminUser = await User.findOne({ email });
+
+    if (adminUser.role !== "admin") {
+      res.status(403).json({
+        error: "Admin resource. Access denied.",
+      });
+    }
+
+    next();
+  } catch (error) {
+    res.status(400).json({
+      error: error.message,
     });
   }
 };
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,12 @@
 const express = require("express");
 
-const { authCheck } = require("../middlewares/auth");
+const { authCheck, adminCheck } = require("../middlewares/auth");
 const { createOrUpdateUser, currentUser } = require("../controllers/auth");
 
 const router = express.Router();
 
 router.post("/create-or-update-user", authCheck, createOrUpdateUser);
 router.get("/current-user", authCheck, currentUser);
+router.get("/current-admin", authCheck, adminCheck, currentUser);
 
 module.exports = router;
